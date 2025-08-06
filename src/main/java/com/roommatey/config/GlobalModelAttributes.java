a6dabefab8ead9003b1ab559596a91e71651ee63
Lui <lsanch78@asu.edu>
2025-08-06 13:24:03 -0700

    global model attributes not working properly, need to read spring boot documentation

--- src/main/java/com/roommatey/config/GlobalModelAttributes.java
+++ src/main/java/com/roommatey/config/GlobalModelAttributes.java
@@ -1,8 +1,9 @@
 package com.roommatey.config;
 
+import com.roommatey.model.Household;
 import com.roommatey.model.RoommateyUserDetails;
+import com.roommatey.model.User;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,14 +14,20 @@ public class GlobalModelAttributes {
 
     // So we could grab the current user logged in globally
     @ModelAttribute
-    public void addLoggedInUserName(Model model, Authentication auth){
+    public void addLoggedInUserAttributes(Model model, Authentication auth){
 
-
-
+        // Grab names globally
         if (auth != null && auth.getPrincipal() instanceof RoommateyUserDetails details){
-            String name = details.getUser().getName();
-            model.addAttribute("userName", name);
+            User user = details.getUser();
+
+
+            model.addAttribute("userName", user.getName());
+
+
+            model.addAttribute("household", user.getHousehold());
         }
+
+
 
     }
 }
